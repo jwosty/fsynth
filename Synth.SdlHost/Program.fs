7a85694a47c53b31f2b67f829c7f878a4c292bb8
jwosty <jwosty@users.noreply.github.com>
2016-09-11 18:20:06 -0600

    Refactoring

--- Synth.SdlHost/Program.fs
+++ Synth.SdlHost/Program.fs
@@ -16,35 +16,56 @@ let rec pollEvents () =
     let mutable event = ref Unchecked.defaultof<_>
     if SDL.SDL_PollEvent event = 0 then [] else !event :: pollEvents ()
 
-let whiteKeySize = 25, 100
-let blackKeySize = 14, 56
+type Vector2<'t> =
+    { x: 't; y: 't }
+    static member ( + ) (v1, v2) = { x = v1.x + v2.x; y = v1.y + v2.y }
+    static member ( - ) (v1, v2) = { x = v1.x - v2.x; y = v1.y - v2.y }
+    static member ( ~-) v = { x = -v.x; y = -v.y }
+let (@@) x y = { x = x; y = y }
+type PianoKey = { natural: bool; position: Vector2<int>; size: Vector2<int>; cutoutWidth1: int; cutoutWidth2: int }
 
-let drawPianoKey renderer ((r, g, b), (x, y), (width, height), cutoutWidth1, cutoutWidth2) =
-    let rect1Start, rect1End = (x + cutoutWidth1, y), (x + width - cutoutWidth2, y + snd blackKeySize)
-    let rect2Start, rect2End = (x, y + snd blackKeySize), (x + width, y + height)
-    if SDL.SDL_SetRenderDrawColor (renderer, r, g, b, 0uy) <> 0 then sdlErr ()
-    let mutable rect1 = new SDL.SDL_Rect(x = fst rect1Start, y = snd rect1Start, w = fst rect1End - fst rect1Start, h = snd rect1End - snd rect1Start)
-    let mutable rect2 = new SDL.SDL_Rect(x = fst rect2Start, y = snd rect2Start, w = fst rect2End - fst rect2Start, h = snd rect2End - snd rect2Start)
-    if SDL.SDL_RenderFillRect (renderer, NativePtr.toNativeInt &&rect1) <> 0 then sdlErr ()
-    if SDL.SDL_RenderFillRect (renderer, NativePtr.toNativeInt &&rect2) <> 0 then sdlErr ()
+let whiteKeySize = 25 @@ 100
+let blackKeySize = 14 @@ 56
+
+let drawLines renderer lines =
+    let lines = lines |> Array.map (fun (x, y) -> new SDL.SDL_Point(x = x, y = y))
+    if SDL.SDL_RenderDrawLines (renderer, lines, lines.Length) <> 0 then sdlErr ()
+
+let fillRect renderer rect =
+    let mutable rect = rect
+    if SDL.SDL_RenderFillRect (renderer, NativePtr.toNativeInt &&rect) <> 0 then sdlErr ()
+
+/// Calculates two bounding boxes that encompass a piano key
+let pianoKeyDimensions pianoKey =
+    let rect1Start = pianoKey.position.x + pianoKey.cutoutWidth1, pianoKey.position.y
+    let rect1End = pianoKey.position.x + pianoKey.size.x - pianoKey.cutoutWidth2, pianoKey.position.y + blackKeySize.y
+    let rect2Start = (pianoKey.position.x, pianoKey.position.y + blackKeySize.y)
+    let rect2End = pianoKey.position.x + pianoKey.size.x, pianoKey.position.y + pianoKey.size.y
+    (rect1Start, rect1End), (rect2Start, rect2End)
+
+let drawPianoKey renderer pianoKey =
+    // rect1 and rect2 are the top (variably sized) and lower (uniform sized) half of the piano key, respectively
+    let (rect1Start, rect1End), (rect2Start, rect2End) = pianoKeyDimensions pianoKey
+    let fill = if pianoKey.natural then 255uy else 64uy
+
+    // rectangles for fill
+    if SDL.SDL_SetRenderDrawColor (renderer, fill, fill, fill, 0uy) <> 0 then sdlErr ()
+    new SDL.SDL_Rect(x = fst rect1Start, y = snd rect1Start, w = fst rect1End - fst rect1Start, h = snd rect1End - snd rect1Start)
+    |> fillRect renderer
+    new SDL.SDL_Rect(x = fst rect2Start, y = snd rect2Start, w = fst rect2End - fst rect2Start, h = snd rect2End - snd rect2Start)
+    |> fillRect renderer
     if SDL.SDL_SetRenderDrawColor (renderer, 0uy, 0uy, 0uy, 0uy) <> 0 then sdlErr ()
-    let stroke =
-        [|rect1Start; fst rect1End, snd rect1Start;
-          rect1End; fst rect2End, snd rect1End;
-          rect2End; fst rect2Start, snd rect2End;
-          rect2Start; fst rect1Start, snd rect2Start;
-          rect1Start|]
-        |> Array.map (fun (x, y) -> new SDL.SDL_Point(x = x, y = y))
-    if SDL.SDL_RenderDrawLines (renderer, stroke, stroke.Length) <> 0 then sdlErr ()
+
+    // lines for outline    
+    [|rect1Start; fst rect1End, snd rect1Start;
+      rect1End; fst rect2End, snd rect1End;
+      rect2End; fst rect2Start, snd rect2End;
+      rect2Start; fst rect1Start, snd rect2Start;
+      rect1Start|]
+    |> drawLines renderer
 
 let drawGui window renderer =
-    //let mutable rect = new SDL.SDL_Rect(x = 10, y = 10, w = 25, h = 100)
-    //let rectPtr = NativePtr.toNativeInt &&rect
-    //if SDL.SDL_SetRenderDrawColor (renderer, 255uy, 255uy, 255uy, 0uy) <> 0 then sdlErr ()
-    //if SDL.SDL_RenderFillRect (renderer, rectPtr) <> 0 then sdlErr ()
-    //if SDL.SDL_SetRenderDrawColor (renderer, 0uy, 0uy, 0uy, 0uy) <> 0 then sdlErr ()
-    //if SDL.SDL_RenderDrawRect (renderer, rectPtr) <> 0 then sdlErr ()
-    drawPianoKey renderer ((255uy, 255uy, 255uy), (50, 20), whiteKeySize, 5, 3)
+    drawPianoKey renderer { natural = true; position = 50 @@ 20; size = whiteKeySize; cutoutWidth1 = 5; cutoutWidth2 = 3 }
 
 let rec runGuiLoop window renderer =
     let events = pollEvents ()
